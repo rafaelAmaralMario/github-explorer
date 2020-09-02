a715cf1a494603bf020720175f11bf25c64fe1de
rafaelmario91 <rafael.mario@objectedge.com>
2020-09-02 13:53:00 -0300

    fix in the dashboard link

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -73,7 +73,7 @@ const Dashboard: React.FC = () => {
         {repositories.map(repository => (
           <Link
             key={repository.full_name}
-            to={`/repository/${repositories.full_name}`}
+            to={`/repository/${repository.full_name}`}
           >
             <img
               src={repository.owner.avatar_url}
